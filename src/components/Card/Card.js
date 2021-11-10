5ce25fff28251a4627c2a7707982f9ac4f003d2f
Mohamed <mohd.ryad@gmail.com>
2021-11-10 16:55:49 -0500

    added some styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,23 +7,34 @@ const Card = ({ id, name, email, pic, company, skill, grades }) => {
     const average = gradesArr => gradesArr.reduce((sum, grade) =>
         parseInt(sum) + parseInt(grade), 0) / gradesArr.length;
     const result = average(grades); // 5
-    return (
-        <Container fluid className="border">
-            <Row className="justify-content">
-                <Col xs lg="auto">
-                    <Image src={pic} width="100" height="100" roundedCircle fluid />
-                </Col>
-                <Col xs lg="auto">
-                    <Row className="">
-                        <h1 align="left">{name}</h1>
-                    </Row>
-                    <Row className="">Email: {email}</Row>
-                    <Row>Company: {company}</Row>
-                    <Row>Skill: {skill}</Row>
-                    <Row>Average: {result}</Row>
-                </Col>
-            </Row>
-        </Container>
+    return ( <
+        Container fluid className = "border" >
+        <
+        Row className = "justify-content" >
+        <
+        Col xs lg = "auto"
+        className = "img-col" >
+        <
+        Image className = "center-block img"
+        src = { pic }
+        width = "100"
+        height = "100"
+        roundedCircle fluid / >
+        <
+        /Col> <
+        Col xs lg = "auto" >
+        <
+        Row className = "" >
+        <
+        h1 align = "left" > { name } < /h1> < /
+        Row > <
+        Row className = "details" > Email: { email } < /Row> <
+        Row className = "details" > Company: { company } < /Row> <
+        Row className = "details" > Skill: { skill } < /Row> <
+        Row className = "details" > Average: { result } < /Row> < /
+        Col > <
+        /Row> < /
+        Container >
     )
 }
 
