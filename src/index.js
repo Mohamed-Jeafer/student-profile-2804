a58e67f83ab8033357ca9e4001c05a244910e3ca
Mohamed Jeafer <mohamed.jeafer@fundmore.ai>
2022-04-01 21:51:55 -0400

    prettier

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import { searchStudents, requestStudents } from './reducers';
-import App from './container/App';
-import reportWebVitals from './reportWebVitals';
-import './components/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { searchStudents, requestStudents } from "./reducers";
+import App from "./container/App";
+import reportWebVitals from "./reportWebVitals";
+import "./components/bootstrap.min.css";
 
-const rootReducer = combineReducers({searchStudents, requestStudents})
-const logger = createLogger()
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger)) 
+const rootReducer = combineReducers({ searchStudents, requestStudents });
+const logger = createLogger();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/reducers.js
+++ src/reducers.js
@@ -1,42 +1,41 @@
 import {
-    CHANGE_SEARCH_FIELD,
-    REQUEST_STUDENTS_FAIL,
-    REQUEST_STUDENTS_SUCCESS,
-    REQUEST_STUDENTS_PENDING
-} from "./constants"
-
+  CHANGE_SEARCH_FIELD,
+  REQUEST_STUDENTS_FAIL,
+  REQUEST_STUDENTS_SUCCESS,
+  REQUEST_STUDENTS_PENDING,
+} from "./constants";
 
 const initialStateSearch = {
-    searchField: '',
-}
+  searchField: "",
+};
 
 const initialSateRequest = {
-    isPending: false,
-    studentsList: [],
-    error: ''
-}
+  isPending: false,
+  studentsList: [],
+  error: "",
+};
 
 export const searchStudents = (state = initialStateSearch, action = {}) => {
-    switch (action.type) {
-        case CHANGE_SEARCH_FIELD:
-            return { ...state, searchField: action.payload }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      return { ...state, searchField: action.payload };
+    default:
+      return state;
+  }
+};
 
 export const requestStudents = (state = initialSateRequest, action = {}) => {
-    switch (action.type) {
-        case REQUEST_STUDENTS_PENDING: {
-            return { ...state, isPending: true }
-        }
-        case REQUEST_STUDENTS_SUCCESS: {
-            return { ...state, studentsList: action.payload, isPending: false }
-        }
-        case REQUEST_STUDENTS_FAIL: {
-            return { ...state, error: action.payload, isPending: false }
-        }
-        default:
-            return state
+  switch (action.type) {
+    case REQUEST_STUDENTS_PENDING: {
+      return { ...state, isPending: true };
     }
-}
+    case REQUEST_STUDENTS_SUCCESS: {
+      return { ...state, studentsList: action.payload, isPending: false };
+    }
+    case REQUEST_STUDENTS_FAIL: {
+      return { ...state, error: action.payload, isPending: false };
+    }
+    default:
+      return state;
+  }
+};
